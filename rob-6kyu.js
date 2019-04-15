0d863cf303d5cf2c23c75da9e9a6e1b8f77bba77
Rob Styles <rob@Zinfandel.local>
2019-04-15 11:09:40 +0100

    first test

---
+++ rob-6kyu.js
@@ -0,0 +1,5 @@
+'use strict'
+
+module.exports = {
+  likes: () => 'no one likes this'
+}
---
+++ rob-6kyu.test.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const { likes } = require('./rob-6kyu')
+
+describe('likes function', () => {
+  it('should say "no one" for an empty array', () => {
+    expect(likes([])).toEqual('no one likes this')
+  })
+
+  it.skip('should', () => {})
+  it.skip('should', () => {})
+  it.skip('should', () => {})
+  it.skip('should', () => {})
+
+  //   likes [] // must be "no one likes this"
+  // likes ["Peter"] // must be "Peter likes this"
+  // likes ["Jacob", "Alex"] // must be "Jacob and Alex like this"
+  // likes ["Max", "John", "Mark"] // must be "Max, John and Mark like this"
+  // likes ["Alex", "Jacob", "Mark", "Max"] // must be "Alex, Jacob and 2 others like this"
+
+  // likes ["Alex", "Jacob", "Mark", "Max", "Foo"] // must be "Alex, Jacob and 3 others like this"
+  // likes ["Alex", "Jacob", "Mark", "Max", "Foo", "Bar"] // must be "Alex, Jacob and 4 others like this"
+  // likes ["Alex", "Jacob", "Mark", "Max", "Foo", "Bar", "Baz"] // must be "Alex, Jacob and 5 others like this"
+})
