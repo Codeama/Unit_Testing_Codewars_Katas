0bda48b51442c1c7d8d252f9afa6549697dc1066
Rob Styles <rob@Zinfandel.local>
2019-04-15 11:21:05 +0100

    fifth and sixth tests

--- rob-6kyu.test.js
+++ rob-6kyu.test.js
@@ -23,10 +23,20 @@ describe('likes function', () => {
     expect(likes(['Foo', 'Bar'])).toEqual('Foo and Bar like this')
   })
 
+  it('should state all three names for three like', () => {
+    expect(likes(['Max', 'John', 'Mark'])).toEqual(
+      'Max, John and Mark like this'
+    )
+  })
+
+  it('should state all three names for three like', () => {
+    expect(likes(['Foo', 'Bar', 'Baz'])).toEqual('Foo, Bar and Baz like this')
+  })
+
+  it.skip('should', () => {})
   it.skip('should', () => {})
   it.skip('should', () => {})
 
-  // likes ["Jacob", "Alex"] // must be "Jacob and Alex like this"
   // likes ["Max", "John", "Mark"] // must be "Max, John and Mark like this"
   // likes ["Alex", "Jacob", "Mark", "Max"] // must be "Alex, Jacob and 2 others like this"
 
